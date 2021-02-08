01916a17af98beeb2864c8fad1bada45c695d930
MakhmudEgo <MakhmudEgo@yandex.ru>
2021-02-08 11:39:42 +0300

    changed it to a template Tree

--- Tree.hpp
+++ Tree.hpp
@@ -8,35 +8,148 @@
 #include <iostream>
 #include <vector>
 
+template<class T>
 class Tree {
 public:
-	Tree(const int&);
-	Tree(const int& index, Tree *child);
-	Tree(const int& index, Tree *child, Tree *parent);
-	Tree(const Tree&);
-	Tree& operator=(const Tree&);
-	virtual ~Tree();
+	explicit Tree(const T&index)
+	: _index(index), _parent(this)
+	{}
+	explicit Tree(const T& index, Tree *child)
+	: _index(index), _parent(this)
+	{
+		if (child)
+		{
+			this->_children.push_back(child);
+			child->_parent = this;
+		}
+	}
+	explicit Tree(const T& index, Tree *child, Tree *parent)
+	: _index(index), _parent(parent)
+	{
+		if (parent)
+		{
+			parent->addChild(this);
+		}
+		if (child)
+		{
+			this->_children.push_back(child);
+			child->_parent = this;
+		}
+	}
+	explicit Tree(const Tree& tree)
+	: _parent(this)
+	{
+		operator=(tree);
+	}
+	Tree& operator=(const Tree& tree)
+	{
+		if (this != &tree)
+		{
+			copyTree(tree, this);
+		}
+		return (*this);
+	}
+	virtual ~Tree()
+	{
+		if (this == this->_parent)
+		{
+			killChildren(this);
+		}
+		if (this == this->_parent)
+		{
+			std::cout << this->_index << " parent dead :(" << std::endl;
+		}
+		else
+		{
+			std::cout << this->_index << " child dead :(" << std::endl;
+		}
+	}
 
 	//getters
-	int getIndex() const;
-	const std::vector<Tree *> &getChildren() const;
-	Tree *getParent() const;
+	int getIndex() const
+	{
+		return (this->_index);
+	}
+	const std::vector<Tree *> &getChildren() const
+	{
+		return (this->_children);
+	}
+	Tree *getParent() const
+	{
+		return (this->_parent);
+	}
 
 	//setters
-	void setIndex(int index);
-	void setChildren(const std::vector<Tree *> &children);
-	void setParent(Tree *parent);
+	void setIndex(int index)
+	{
+		this->_index = index;
+	}
+	void setChildren(const std::vector<Tree *> &children)
+	{
+		for (int i = 0; i < children.size(); ++i)
+		{
+			this->_children.push_back(children[i]);
+		}
+	}
+	void setParent(Tree *parent)
+	{
+		if (parent)
+		{
+			this->_parent = parent;
+			parent->addChild(this);
+		}
+	}
+	void addChild(Tree *newChild)
+	{
+		if (newChild)
+		{
+			newChild->_parent = this;
+			this->_children.push_back(newChild);
+		}
+	}
 
-	void addChild(Tree *newChild);
 private:
-	int _index;
+	T _index;
 	std::vector<Tree*> _children;
 	Tree *_parent;
 
-	void killChildren(Tree *parent);
-	void copyTree(const Tree& tree, Tree *parent);
+	void killChildren(Tree *parent)
+	{
+		for (int i = 0; i < parent->_children.size(); ++i)
+		{
+			if (!parent->_children.empty())
+			{
+				killChildren(parent->_children[i]);
+			}
+			delete parent->_children[i];
+		}
+	}
+	void copyTree(const Tree& tree, Tree *parent)
+	{
+		if (parent == this)
+		{
+			this->_index = tree._index;
+		}
+		for (int i = 0; i < tree._children.size(); ++i)
+		{
+			Tree *top = new Tree(tree._children[i]->_index, nullptr, parent);
+			if (!tree._children[i]->_children.empty())
+			{
+				copyTree(*tree._children[i], top);
+			}
+		}
+	}
 };
 
-int heightTree(const Tree *parent);
+template<class T>
+int heightTree(const Tree<T> *parent)
+{
+	int height = 1;
+	for (int i = 0; i < parent->getChildren().size(); ++i)
+	{
+		height = 1 + heightTree(parent->getChildren()[i]);
+	}
+	return (height);
+}
 
 #endif //TREE_HPP
--- main.cpp
+++ main.cpp
@@ -10,32 +10,35 @@ int main()
 	std::cout << std::endl << "Tree" << std::endl << std::endl;
 	{
 
-		Tree *tree1 = new Tree(1);
+		Tree<int> *tree1 = new Tree<int>(1);
 		std::cout << "heightTree : " << heightTree(tree1) << std::endl;
 
-		Tree *tree2 = new Tree(2);
+		Tree<int> *tree2 = new Tree<int>(2);
 		tree1->addChild(tree2);
 		std::cout << "heightTree : " << heightTree(tree1) << std::endl;
 
-		Tree *tree3 = new Tree(3);
+		Tree<int> *tree3 = new Tree<int>(3);
 		tree2->addChild(tree3);
 		std::cout << "heightTree : " << heightTree(tree1) << std::endl;
 
-		Tree *tree4 = new Tree(4);
+		Tree<int> *tree4 = new Tree<int>(4);
 		tree2->addChild(tree4);
-		tree4->addChild(new Tree(5, new Tree(6, new Tree(7))));
+		tree4->addChild(new Tree<int>(5, new Tree<int>(6, new Tree<int>(7))));
 		std::cout << "heightTree : " << heightTree(tree1) << std::endl;
 
-		tree2->addChild(new Tree(8));
-		tree2->addChild(new Tree(9));
-		tree2->addChild(new Tree(10));
-		tree2->addChild(new Tree(11));
-		tree2->addChild(new Tree(12));
-		tree2->addChild(new Tree(13));
-		Tree newTree(1);
+		tree2->addChild(new Tree<int>(8));
+		tree2->addChild(new Tree<int>(9));
+		tree2->addChild(new Tree<int>(10));
+		tree2->addChild(new Tree<int>(11));
+		tree2->addChild(new Tree<int>(12));
+		tree2->addChild(new Tree<int>(13));
+
+		Tree<std::string> stringTree("parent", new Tree<std::string>("child1", new Tree<std::string>("child2", new Tree<std::string>("child3"))));
+
+		Tree<int> newTree(1);
 		newTree = *tree1;
 
-		Tree *copyTree = new Tree(newTree);
+		Tree<int> *copyTree = new Tree<int>(newTree);
 
 		delete copyTree;
 		delete tree1;
